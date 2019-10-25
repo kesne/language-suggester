cc62245065882752ca631d6257de8ac0b40dea85
Jaime Gensler <jaimegensler0@gmail.com>
2019-10-25 16:39:44 -0700

    create counter variables, create scoreCount function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+var rubyCount = 0;
+var javascriptCount = 0;
+var pythonCount = 0;
+var goCount = 0;
+var javaCount = 0;
+var csharpCount = 0;
+var cplusplusCount = 0;
+
+function scoreCount(inputCheck) {
+  if (inputCheck === "ruby") {
+    rubyCount += 1;
+  } else if (inputCheck === "javascript") {
+    javascriptCount += 1;
+  } else if (inputCheck === "python") {
+    pythonCount += 1;
+  } else if (inputCheck === "go") {
+    goCount += 1;
+  } else if (inputCheck === "java") {
+    javaCount += 1;
+  } else if (inputCheck === "c#") {
+    csharpCount += 1;
+  } else if (inputCheck === "c++") {
+    cplusplusCount += 1;
+  } else {
+    console.log("Error - scoreCount input does not match to any languages.")
+  }
+}
+
 $(document).ready(function () {
 
   $("button#start").click(function() {
@@ -6,7 +34,7 @@ $(document).ready(function () {
   });
 
   $("form#userInfo").submit(function() {
-    
+
     var projectInput = $("input:radio[name=project]:checked").val();
     var planInput = $("input:radio[name=plan]:checked").val();
     var animalInput = $("input:radio[name=animal]:checked").val();
