1544d74afa82b257afe9cbcb87083243d160881a
Jaime Gensler <jaimegensler0@gmail.com>
2019-10-25 14:15:18 -0700

    update question text for most of survey, add 'finish survey' button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
+
+
 $(document).ready(function () {
   $("button#start").click(function() {
     $("button#start").toggle("slow");
     $("div#survey").delay(600).slideToggle(1000);
   });
+  $("button#finish").click(function() {
+    $("div#survey").slideToggle(1000);
+  })
 })
