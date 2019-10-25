3d8c65fab405c1c6dfafa0e33e2402a99d333f8f
Jaime Gensler <jaimegensler0@gmail.com>
2019-10-25 16:09:57 -0700

    fix form submission bug, form will now correctly submit and grab answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
-var result = "";
-var projectInput = "";
-var planInput = "";
-var animalInput = "";
-var drinkInput = "";
-var musicInput = "";
-var gemInput = "";
-
 $(document).ready(function () {
 
   $("button#start").click(function() {
-    // $("button#start").toggle("slow");
+    $("button#start").toggle("slow");
     $("div#survey").delay(600).slideToggle(1000);
-    alert("this works");
   });
 
-  alert("this too");
-
-  $("button#finish").click(function(event) {
-    alert("Got here");
+  $("form#userInfo").submit(function() {
+    
     var projectInput = $("input:radio[name=project]:checked").val();
     var planInput = $("input:radio[name=plan]:checked").val();
     var animalInput = $("input:radio[name=animal]:checked").val();
     var drinkInput = $("input:radio[name=drink]:checked").val();
     var musicInput = $("input:radio[name=music]:checked").val();
     var gemInput = $("input:radio[name=gem]:checked").val();
+
     console.log(projectInput, planInput, animalInput, drinkInput, musicInput, gemInput);
 
-    // $("div#survey").delay(1000).slideToggle(1000);
     event.preventDefault();
 
-  alert("does this though?");
-
   });
+
 });
