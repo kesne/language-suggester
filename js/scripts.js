51eccbfe2d28b261764a95583752141687507e77
Jordan Gensler <jgensler@netflix.com>
2019-10-29 21:03:06 -0700

    Baby sharp do do do do do do

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,111 @@
-//Scoreboard's indexes represent languages in this order:
-// ruby, js, python, go, java, c#, c++
-var scoreboard = [0, 0, 0, 0, 0, 0, 0];
-var winner = "";
-var runnerUp = "";
+function scoreCount(scoreboard, questionValue) {
+    if (!(questionValue in scoreboard)) {
+        console.log(questionValue, scoreboard);
+        throw new Error('Error in scoreCount - questionValue is not a language.');
+    }
 
-function scoreCount(questionValue) {
-  if (questionValue === "ruby") {
-    scoreboard[0] += 1;
-  } else if (questionValue === "javascript") {
-    scoreboard[1] += 1;
-  } else if (questionValue === "python") {
-    scoreboard[2] += 1;
-  } else if (questionValue === "go") {
-    scoreboard[3] += 1;
-  } else if (questionValue === "java") {
-    scoreboard[4] += 1;
-  } else if (questionValue === "c#") {
-    scoreboard[5] += 1;
-  } else if (questionValue === "c++") {
-    scoreboard[6] += 1;
-  } else {
-    console.log("Error in scoreCount - questionValue is not a language.");
-  }
+    return {
+        ...scoreboard,
+        [questionValue]: scoreboard[questionValue] + 1
+    };
 }
 
-function indexInterpreter(indexNumber) {
-  if (indexNumber === 0) {
-    return "Ruby";
-  } else if (indexNumber === 1) {
-    return "JavaScript";
-  } else if (indexNumber === 2) {
-    return "Python";
-  } else if (indexNumber === 3) {
-    return "Go";
-  } else if (indexNumber === 4) {
-    return "Java";
-  } else if (indexNumber === 5) {
-    //using # (hash) creates issues with image retrieval later, ♯ (sharp) does not
-    return "C♯";
-  } else if (indexNumber === 6) {
-    return "C++";
-  } else {
-    console.log("Error in indexInterpreter - indexNumber not in array.");
-  }
+const keyNameToBetterLanguageName = {
+    ruby: 'Ruby',
+    javascript: 'JavaScript',
+    python: 'Python',
+    go: 'Go',
+    java: 'Java',
+    'csharp': 'C♯',
+    'cpp': 'C++'
+};
+
+function betterName(languageName) {
+    return keyNameToBetterLanguageName[languageName];
 }
 
-function finalResult() {
-  var winnerIndex = scoreboard.indexOf(Math.max(...scoreboard));
-  scoreboard[winnerIndex] = 0;
-  var runnerUpIndex = scoreboard.indexOf(Math.max(...scoreboard));
-  winner = indexInterpreter(winnerIndex);
-  runnerUp = indexInterpreter(runnerUpIndex);
+function finalResult(scoreboard) {
+    const results = Object.entries(scoreboard).sort((a, b) => {
+        if (a[1] > b[1]) {
+            return -1;
+        } else if (b[1] > a[1]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    return {
+        winner: results[0][0],
+        runnerUp: results[1][0]
+    };
 }
 
 function wikiURL(languageName) {
-  var wikiLink = "https://en.wikipedia.org/wiki/"
-  if (languageName === "C♯") {
-    wikiLink += "C_Sharp_(programming_language)";
-  } else if (languageName === "JavaScript" || languageName === "C++") {
-    wikiLink += languageName;
-  } else {
-    wikiLink = wikiLink + languageName + "_(programming_language)"
-  }
-  return wikiLink;
+    var wikiLink = 'https://en.wikipedia.org/wiki/';
+    if (languageName === 'csharp') {
+        wikiLink += 'C_Sharp_(programming_language)';
+    } else if (languageName === 'javascript') {
+        wikiLink += languageName;
+    } else if (languageName === 'cpp') {
+        wikiLink += 'C++';
+    } else {
+        wikiLink = wikiLink + languageName + '_(programming_language)';
+    }
+    return wikiLink;
 }
 
-
 //Front-End Logic
-$(document).ready(function () {
+$(document).ready(function() {
+    $('button#start').click(function() {
+        $('button#start').toggle(400);
+        $('div#survey')
+            .delay(400)
+            .slideToggle(1000);
+    });
 
-  $("button#start").click(function() {
-    $("button#start").toggle(400);
-    $("div#survey").delay(400).slideToggle(1000);
-  });
+    $('form#userInfo').submit(function() {
+        event.preventDefault();
 
-  $("form#userInfo").submit(function() {
-    event.preventDefault();
+        var projectInput = $('input:radio[name=project]:checked').val();
+        var planInput = $('input:radio[name=plan]:checked').val();
+        var animalInput = $('input:radio[name=animal]:checked').val();
+        var drinkInput = $('input:radio[name=drink]:checked').val();
+        var musicInput = $('input:radio[name=music]:checked').val();
+        var gemInput = $('input:radio[name=gem]:checked').val();
 
-    var projectInput = $("input:radio[name=project]:checked").val();
-    var planInput = $("input:radio[name=plan]:checked").val();
-    var animalInput = $("input:radio[name=animal]:checked").val();
-    var drinkInput = $("input:radio[name=drink]:checked").val();
-    var musicInput = $("input:radio[name=music]:checked").val();
-    var gemInput = $("input:radio[name=gem]:checked").val();
+        const scoreboard = [projectInput, planInput, animalInput, drinkInput, musicInput, gemInput].reduce(
+            (scoreboard, input) => {
+                return scoreCount(scoreboard, input);
+            },
+            {
+                ruby: 0,
+                javascript: 0,
+                python: 0,
+                go: 0,
+                java: 0,
+                'csharp': 0,
+                'cpp': 0
+            }
+        );
 
-    scoreCount(projectInput);
-    scoreCount(planInput);
-    scoreCount(animalInput);
-    scoreCount(drinkInput);
-    scoreCount(musicInput);
-    scoreCount(gemInput);
+        const result = finalResult(scoreboard);
+        const winner = result.winner;
+        const runnerUp = result.runnerUp;
 
-    finalResult();
+        //fill result panels
+        $('span.nameInsert').text($('input#userName').val());
+        $('#option1 span').text(betterName(winner));
+        $('#option1 img').attr('src', 'img/' + winner.toLowerCase() + '.png');
+        $('#option1 a').attr('href', wikiURL(winner));
+        $('#option2 span').text(betterName(runnerUp));
+        $('#option2 img').attr('src', 'img/' + runnerUp.toLowerCase() + '.png');
+        $('#option2 a').attr('href', wikiURL(runnerUp));
 
-    //fill result panels
-    $("span.nameInsert").text($("input#userName").val());
-    $("#option1 span").text(winner);
-    $("#option1 img").attr("src", "img/" + winner.toLowerCase() + ".png");
-    $("#option1 a").attr("href", wikiURL(winner));
-    $("#option2 span").text(runnerUp);
-    $("#option2 img").attr("src", "img/" + runnerUp.toLowerCase() + ".png");
-    $("#option2 a").attr("href", wikiURL(runnerUp));
-
-    //reveal result panels
-    $("div#survey").slideToggle(1000);
-    $("div#result").delay(1000).slideToggle(1000);
-  });
-
+        //reveal result panels
+        $('div#survey').slideToggle(1000);
+        $('div#result')
+            .delay(1000)
+            .slideToggle(1000);
+    });
 });
